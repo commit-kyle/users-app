a0bcc977c44c84f8dbe7b4e7e8158abf21ca58b9
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-27 21:35:27 +0200

    Added custom Button component to form

--- users-app/src/components/Users/AddUser/AddUser.js
+++ users-app/src/components/Users/AddUser/AddUser.js
@@ -1,16 +1,23 @@
+import classes from './AddUser.module.css';
+
+import Card from '../../UI/Card/Card';
+import Button from '../../UI/Button/Button';
+
 const AddUser = props => {
 	const AddUserHandler = e => {
 		e.preventDefault();
 	};
 
 	return (
-		<form onSubmit={AddUserHandler}>
-			<label htmlFor="username">Username</label>
-			<input id="username" type="text" />
-			<label htmlFor="age">Age (Years)</label>
-			<input id="age" type="text" />
-			<button type="submit">Add User</button>
-		</form>
+		<Card className={classes.input}>
+			<form onSubmit={AddUserHandler}>
+				<label htmlFor="username">Username</label>
+				<input id="username" type="text" />
+				<label htmlFor="age">Age (Years)</label>
+				<input id="age" type="text" />
+				<Button type="submit">Add User</Button>
+			</form>
+		</Card>
 	);
 };
 
