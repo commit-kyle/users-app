9e69e8743554d242714415d9e46cd0acfbd829a2
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-27 20:45:23 +0200

    Configured form structure for adding a user

--- users-app/src/components/Users/AddUser/AddUser.js
+++ users-app/src/components/Users/AddUser/AddUser.js
@@ -1,9 +1,17 @@
 const AddUser = props => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const AddUserHandler = e => {
+		e.preventDefault();
+	};
 
-export default AddUser
+	return (
+		<form onSubmit={AddUserHandler}>
+			<label htmlFor="username">Username</label>
+			<input id="username" type="text" />
+			<label htmlFor="age">Age (Years)</label>
+			<input id="age" type="text" />
+			<button type="submit">Add User</button>
+		</form>
+	);
+};
+
+export default AddUser;
