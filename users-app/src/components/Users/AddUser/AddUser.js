412c5299807ffb2d60bfa22ee4e29515fdfb2a15
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-27 21:46:49 +0200

    Configured form state management and data retrieval from inputs

--- users-app/src/components/Users/AddUser/AddUser.js
+++ users-app/src/components/Users/AddUser/AddUser.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import classes from './AddUser.module.css';
 
 import Card from '../../UI/Card/Card';
 import Button from '../../UI/Button/Button';
 
 const AddUser = props => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('')
+
+  const usernameChangeHandler = e => {
+    setUsername(e.target.value);
+  };
+
+  const ageChangeHandler = e => {
+    setAge(e.target.value);
+  };
+
 	const AddUserHandler = e => {
 		e.preventDefault();
 	};
@@ -12,9 +25,9 @@ const AddUser = props => {
 		<Card className={classes.input}>
 			<form onSubmit={AddUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input id="username" type="text" />
+				<input id="username" type="text" onChange={usernameChangeHandler} />
 				<label htmlFor="age">Age (Years)</label>
-				<input id="age" type="text" />
+				<input id="age" type="text" onChange={ageChangeHandler}/>
 				<Button type="submit">Add User</Button>
 			</form>
 		</Card>
