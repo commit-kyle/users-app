a44bb5162b50d692b3c4c72a36452e1760d50ad0
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-28 13:48:18 +0200

    Component imports. Configured state management for user data provided

--- users-app/src/App.js
+++ users-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import AddUser from './components/Users/AddUser/AddUser';
+import UsersList from './components/UsersList/UsersList';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const addUserHandler = (username, age) => {
+    setUsers(prevUsers => {
+      return [...prevUsers, {name: username, age: age}]
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={users} />
     </div>
   );
 }
