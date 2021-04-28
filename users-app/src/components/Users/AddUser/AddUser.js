f2020fca95bb003578ceed9dfed535a6fd2c6491
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-28 14:54:52 +0200

    Reconfigured validation logic to handle more specific input errors, and defined error state management. Providing detailed error data for the imported ErrorModal component

--- users-app/src/components/Users/AddUser/AddUser.js
+++ users-app/src/components/Users/AddUser/AddUser.js
@@ -4,10 +4,12 @@ import classes from './AddUser.module.css';
 
 import Card from '../../UI/Card/Card';
 import Button from '../../UI/Button/Button';
+import ErrorModal from '../../UI/ErrorModal/ErrorModal';
 
 const AddUser = props => {
 	const [username, setUsername] = useState('');
 	const [age, setAge] = useState('');
+	const [error, setError] = useState();
 
 	const usernameChangeHandler = e => {
 		setUsername(e.target.value);
@@ -17,37 +19,69 @@ const AddUser = props => {
 		setAge(e.target.value);
 	};
 
-	const AddUserHandler = e => {
+	const addUserHandler = e => {
 		e.preventDefault();
-		
-    if (username.trim().length === 0 || age.trim().length === 0) {
-			return;
-    }
-    if (+age < 1) {
-			return;
-    }
-		
-		props.onAddUser(username, age)
 
-    setUsername('');
-    setAge('');
+		if (username.trim().length === 0 && age.trim().length === 0) {
+			setError({
+				title: 'Invalid details',
+				message: 'Please provide your name and age to continue.'
+			})
+			return;
+		} else if (username.trim().length === 0) {
+			setError({
+				title: 'Invalid username',
+				message: 'Please provide a valid name to continue.'
+			})
+			return;
+		} else if (age.trim().length === 0) {
+			setError({
+				title: 'Invalid age',
+				message: 'Please provide your age to continue.'
+			})
+			return;
+		} else if (+age < 1) {
+			setError({
+				title: 'Invalid age',
+				message: 'Please provide a valid age (more than zero) to continue.'
+			})
+			setAge('');
+			return;
+		}
+
+		props.onAddUser(username, age);
+
+		setUsername('');
+		setAge('');
 	};
 
+	const onErrorHandler = () => {
+		setError(null);
+	}
+
 	return (
-		<Card className={classes.input}>
-			<form onSubmit={AddUserHandler}>
-				<label htmlFor="username">Username</label>
-				<input
-					id="username"
-					type="text"
-					onChange={usernameChangeHandler}
-					value={username}
-				/>
-				<label htmlFor="age">Age (Years)</label>
-				<input id="age" type="number" onChange={ageChangeHandler} value={age} />
-				<Button type="submit">Add User</Button>
-			</form>
-		</Card>
+		<div>
+			{error && <ErrorModal title={error.title} message={error.message} onConfirm={onErrorHandler}/>}
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor="username">Username</label>
+					<input
+						id="username"
+						type="text"
+						onChange={usernameChangeHandler}
+						value={username}
+					/>
+					<label htmlFor="age">Age (Years)</label>
+					<input
+						id="age"
+						type="number"
+						onChange={ageChangeHandler}
+						value={age}
+					/>
+					<Button type="submit">Add User</Button>
+				</form>
+			</Card>
+		</div>
 	);
 };
 
