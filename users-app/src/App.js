98615c30f743999b3f9e5dee642a02219183e73a
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-28 16:55:30 +0200

    Fixed user rendering error

--- users-app/src/App.js
+++ users-app/src/App.js
@@ -4,20 +4,24 @@ import AddUser from './components/Users/AddUser/AddUser';
 import UsersList from './components/UsersList/UsersList';
 
 function App() {
-  const [users, setUsers] = useState()
+	const [users, setUsers] = useState();
 
-  const addUserHandler = (username, age) => {
-    setUsers(prevUsers => {
-      return [...prevUsers, {name: username, age: age}]
-    })
-  }
+	const addUserHandler = (username, age) => {
+		setUsers(prevUsers => {
+			if (prevUsers) {
+				return [...prevUsers, { name: username, age: age }];
+			} else {
+				setUsers([{ name: username, age: age }]);
+			}
+		});
+	};
 
-  return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={users} />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={users} />
+		</Fragment>
+	);
 }
 
 export default App;
